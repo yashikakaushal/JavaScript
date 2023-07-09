701e74e25f85994b5ad1926079055cf3688d3446
Yashika_Kaushal <122152548+yashikakaushal@users.noreply.github.com>
2023-07-09 16:03:34 +0100

    ADD JAVASCRIPT COMMIT

---
+++ Ternary.js
@@ -0,0 +1,52 @@
+////////////////////// Ternary Operator//////////////////////
+/*example-1*/
+var grade = 42;
+
+grade < 45 
+? console.log('you can not go to next class') 
+: console.log('you can go to next class')
+
+/*example-2*/
+var grade = 80;
+
+grade < 45 
+? console.log('you can not go to next class') 
+: console.log('you can go to next class')
+
+/* example-3*/
+var grade = 80;
+
+grade < 45 
+? console.log('you can not go to next class') 
+: console.log('you can go to next class')
+
+var result = grade < 45 ? 'failure' : 'success';
+console.log(result);
+
+/* example-4*/
+var grade = 42;
+
+grade < 45 
+? console.log('you can not go to next class') 
+: console.log('you can go to next class')
+
+var result = grade < 45 ? 'failure' : 'success';
+console.log(result);
+
+/* example-5.*/
+var grade = 42;
+
+grade < 45 
+? console.log('you can not go to next class') 
+: console.log('you can go to next class')
+
+var result = grade < 45 ? 'failure' : 'success';
+console.log(result);
+
+if(grade < 45)
+{
+    result = 'failure'
+}
+else{
+    result = 'success'
+}
