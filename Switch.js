4387ecba497df3d41959454a4d105cecfceb0809
Yashika_Kaushal <122152548+yashikakaushal@users.noreply.github.com>
2023-07-10 13:24:02 +0100

    ADD JAVASCRIPT COMMIT

---
+++ Switch.js
@@ -0,0 +1,38 @@
+/////////////////////// Switch Statement//////////////////////
+/* ex-1*/
+var day;
+day = 'friday';
+
+switch(day){
+    case 'saturday':
+        console.log('I Like Saturday');
+        break;
+    case 'monday':
+            console.log('I do not Like Monday');
+            break;
+    case 'friday':
+            console.log('I really Like friday');
+
+    default:
+        console.log('I do not know the day');
+    
+}
+
+/* ex-2*/
+var day;
+day = 'monday';
+
+switch(day){
+    case 'saturday':
+        console.log('I Like Saturday');
+        break;
+    case 'monday':
+        console.log('I do not Like Monday');
+        break;
+    case 'friday':
+        console.log('I really Like friday');
+        break;
+    default:
+        console.log('I do not know the day');
+        break;
+}
