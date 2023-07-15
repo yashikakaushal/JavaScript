755ba983e7835c0f4609b3784486149ebb8b8218
Yashika_Kaushal <122152548+yashikakaushal@users.noreply.github.com>
2023-07-15 15:41:45 +0100

    ADD JAVASCRIPT COMMIT

---
+++ javascript1.js
@@ -0,0 +1,28 @@
+var name =prompt("what is your name");
+var firstChar = name.slice(0,1);
+
+var name = console.log("hello");
+var number = console.log("2+3");
+
+var MyName="angela";
+var yourName=prompt("whta is your name");
+console.log("what is your"+ "MyName"+" , Welcome your "+ "number");
+
+
+
+var a= "20";
+var b="30";
+
+console.log("a is :"+a);
+console.log("b is:"+b);
+
+
+var gamelevel = 1;
+gamelevel = 2;
+gamelevel = 3;
+console.log(gamelevel);
+
+var Myname = 'yashika';
+var hduweuewhjje = 'kaushal';
+console.log(Myname);
+console.log(hduweuewhjje);
