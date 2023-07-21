a1066da5d512d7865e3d952f7431cbc104673fed
Yashika_Kaushal <122152548+yashikakaushal@users.noreply.github.com>
2023-07-21 15:01:43 +0100

    ADD JAVASCRIPT COMMIT

---
+++ conditional.js
@@ -0,0 +1,134 @@
+/*example-1*/
+var grade;
+grade = 38;
+
+if (grade < 40 )
+{
+console.log("you can not go to next class");
+}
+else if (grade >= 40 && grade < 45 )
+{
+    console.log('you can not go to next class but you can enter the exam');
+}
+    
+else
+{
+console.log('you can go to next class');
+}
+
+/*example-2*/
+var grade;
+grade = 50;
+
+if (grade < 40 )
+{
+console.log("you can not go to next class");
+}
+else if (grade >= 40 && grade < 45 )
+{
+    console.log('you can not go to next class but you can enter the exam');
+}
+    
+else
+{
+console.log('you can go to next class');
+}
+
+/*example-3*/
+var grade;
+grade = 42;
+
+if (grade < 40 )
+{
+console.log("you can not go to next class");
+}
+else if (grade >= 40 && grade < 45 )
+{
+    console.log('you can not go to next class but you can enter the exam');
+}
+    
+else
+{
+console.log('you can go to next class');
+}
+
+/*example-4*/
+var grade;
+grade = 40;
+
+if (grade < 40 )
+{
+console.log("you can not go to next class");
+}
+else if (grade >= 40 && grade < 45 )
+{
+    console.log('you can not go to next class but you can enter the exam');
+}
+    
+else
+{
+console.log('you can go to next class');
+}
+
+/*example-1*/
+/*
+var age;
+age = 16;
+
+if(age < 18)
+{
+    console.log('you can not enter the cafe ');      
+}
+else if(age < 23)
+{
+    console.log('you can enter the cafe but can not a buy drink ');      
+}
+else{
+    console.log('you can enter the cafe and can buy a drink');
+}
+*/
+
+/*example-2*/
+var age;
+age = 16;
+
+if(age < 18)
+{
+    console.log('you can not enter the cafe ');      
+}
+ if(age < 23)
+{
+    console.log('you can enter the cafe but can not a buy drink ');      
+}
+else{
+    console.log('you can enter the cafe and can buy a drink');
+}
+
+/*example-3*/
+var age;
+age = 16;
+
+if(age < 18)
+{
+    console.log('you can not enter the cafe ');      
+}
+ if( age >= 18 && age < 23)
+{
+    console.log('you can enter the cafe but can not a buy drink ');      
+}
+
+/*example-4*/
+var age;
+age = 16;
+
+if(age < 18)
+{
+    console.log('you can not enter the cafe ');      
+}
+else  if(age < 23)
+{
+    console.log('you can enter the cafe but can not a buy drink ');      
+}
+else{
+    console.log('you can enter the cafe and can buy a drink');
+}
---
+++ language.js
@@ -0,0 +1,91 @@
+////////////////////// JavaScript language basic  //////////////////////
+console.log("HELLO WORLD");
+
+var name="yashika";
+console.log("name");
+
+var age="21";
+console.log("age");
+
+var schoolteam = "basketball";
+var schoolnumber = "20";
+
+var isRegistered = "true";
+console.log(" isRegistered");
+
+var job;
+console.log("job");
+
+job = "student";
+console.log(job);
+
+console.log(name + '' + schoolnumber);
+console.log(age + '' + schoolteam);
+
+var schoolname,hascourse;
+
+schoolname = "OKAYAcademy";
+hascourse = "true";
+
+console.log(schoolname + 'has 35 courses.Dose it have any courses?' + hascourse);
+
+schoolnumber = "twenty-two";
+console.log(schoolnumber);
+
+schoolteam = "tennis";
+console.log(schoolteam);
+
+alert(schoolteam);
+
+var age = prompt("How old is he?");
+console.log('alex is' + age + 'year old');
+
+////////////////////// document //////////////////////
+
+var age = prompt("How old is he?");
+document.write('alex is' + age + 'year old');
+
+////////////////////// BASIC OPERATOR //////////////////////
+
+var totalDistance = 20;
+var distanceAlex = totalDistance - 12;
+console.log(distanceAlex);
+console.log(distanceAlex /2);
+
+var distanceSam = totalDistance + 7;
+console.log(distanceSam);
+console.log(distanceSam *2);
+console.log(distanceSam /2);
+
+/*
+var  totalDistance,distanceAlex,distanceSam;
+ totalDistance = 20;
+ distanceAlex = totalDistance - 12;
+console.log(distanceAlex);
+
+distanceSam = totalDistance + 7;
+console.log(distanceSam);
+*/
+
+/*example-1*/
+var distancetohome,distancetoschool;
+
+distancetohome = 20;
+distancetoschool = 17;
+var iscloser = distancetohome < distancetoschool;
+console.log(iscloser)
+
+/*example-2*/
+var distancetohome,distancetoschool;
+
+distancetohome = 100;
+distancetoschool = 50;
+
+var iscloser = distancetohome > distancetoschool;
+console.log(iscloser);
+
+////////////////////// BOOLEAR LOGIC //////////////////////
+
+var isLoggedIn = false;
+
+
